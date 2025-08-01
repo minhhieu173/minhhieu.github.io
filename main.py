699ced131fcc303279ca09b4e4c1eaa67e6db147
minhhieu173 <74543180+minhhieu173@users.noreply.github.com>
2025-08-02 00:23:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import streamlit as st
 import pandas as pd
-import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import MinMaxScaler
+
 
 
 
