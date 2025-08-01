fdad331a68e2c5eb40e15b66aa80eb4d5548620f
minhhieu173 <74543180+minhhieu173@users.noreply.github.com>
2025-08-02 00:20:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.preprocessing import MinMaxScaler
+
 
 
 st.title("📊 ABC Company: Revenue Analysis & Forecasting")
